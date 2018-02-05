6a4cd42c35c0a1213052a96601cfe23551cf13b4
valentin <valentin.escamilla@outlook.fr>
2018-02-05 12:31:24 +0100

    Add point class

---
+++ API/src/main/java/org/atelier2/entity/Point.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.atelier2.entity;
+
+/**
+ *
+ * @author vali
+ */
+public class Point {
+    private long id;
+    private float lat;
+    private float lng;
+    private String photo;
+    private String description;
+    private int difficulte;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLng() {
+        return lng;
+    }
+
+    public void setLng(float lng) {
+        this.lng = lng;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDifficulte() {
+        return difficulte;
+    }
+
+    public void setDifficulte(int difficulte) {
+        this.difficulte = difficulte;
+    }
+    
+    
+}
