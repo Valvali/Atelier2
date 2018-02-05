322fd8535a3e19b9c9ba75d3a12e607b082d64f1
Issam_MEDJMADJ <issam21000@live.fr>
2018-02-05 13:36:50 +0100

    baseLayout

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from '@/components/Home'
 import store from '@/store'
 
 
@@ -8,7 +8,11 @@ Vue.use(Router)
 
 export const router = new Router({
   routes: [
-
+  	{
+    	path: '/Home',
+    	name: 'home',
+    	component: Home
+    }
 
   ]
 })
