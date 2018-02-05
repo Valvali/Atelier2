9e93e8aa854cef2b96dd91cba394b7702958f021
Charles BISELX <cha.biselx@gmail.com>
2018-02-05 12:46:16 +0100

    add leaflet's map

---
+++ frontend/src/components/content_test/geolocalisation.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="content_test">
+      <v-map id="map" :zoom="zoom" :center="center">
+        <v-tilelayer :url="url" ></v-tilelayer>
+        <v-marker :lat-lng="marker"></v-marker>
+      </v-map>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import api from '@/services/api'
+
+import Vue2Leaflet from 'vue2-leaflet';
+
+Vue.component('v-map', Vue2Leaflet.Map);
+Vue.component('v-tilelayer', Vue2Leaflet.TileLayer);
+Vue.component('v-marker', Vue2Leaflet.Marker);
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      zoom: 13,
+      center: [47.413220, -1.219482],
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      marker: L.latLng(48.56420, -1.219482),
+    };
+	},
+  methods: {
+
+  },
+  created: function () {
+
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "~leaflet/dist/leaflet.css";
+#map {
+  background-color: red;
+  height: 800px;
+  width : 800px;
+}
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,12 +3,17 @@ import Router from 'vue-router'
 
 import store from '@/store'
 
+import geoloc from '@/components/content_test/geolocalisation'
 
 Vue.use(Router)
 
 export const router = new Router({
   routes: [
-
+    {
+      path: '/geolocation',
+      name: 'geoloc',
+      component: geoloc,
+    },
 
   ]
 })
