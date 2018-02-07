570075d13af2d8cc88340de175f4cb8ca113d1bd
valentin <valentin.escamilla@outlook.fr>
2018-02-07 13:50:56 +0100

    Add UserResource and user route

---
+++ API/src/main/java/org/api/boundary/UserResource.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.api.boundary;
+
+import java.net.URI;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import org.api.entity.User;
+
+/**
+ *
+ * @author vali
+ */
+@Stateless
+@Path("user")
+public class UserResource {
+    
+    @Inject
+    UserManager um;
+    
+    @GET
+    public Response GetUser() {
+        GenericEntity<List<User>> liste = new GenericEntity<List<User>>(this.um.findAll()) {
+        };
+        return Response.ok(liste).build();
+    }
+    
+    @POST
+    public Response newScore(@Valid User u, @Context UriInfo uriInfo) {
+        User newOne = this.um.save(u);
+        String id = newOne.getId();
+        URI uri = uriInfo.getAbsolutePathBuilder().path("/"+id).build();
+        return Response.created(uri).build();
+    }
+    
+    @DELETE
+    @Path("{id}")
+    public Response suppression(@PathParam("id") String id) {
+        this.um.delete(id);
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+}
