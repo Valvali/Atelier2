efdeb982a31e7ece033a5451d21e3b780ed431e4
valentin <valentin.escamilla@outlook.fr>
2018-02-07 15:44:10 +0100

    Add consume/produce

--- API/src/main/java/org/api/boundary/UserResource.java
+++ API/src/main/java/org/api/boundary/UserResource.java
@@ -14,13 +14,16 @@ import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.validation.Valid;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
@@ -32,6 +35,8 @@ import org.api.entity.User;
  */
 @Stateless
 @Path("user")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
     
     @Inject
