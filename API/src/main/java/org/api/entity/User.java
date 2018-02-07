cc956b922c9f3b7b6382d03c1e3a1edee281ca7a
valentin <valentin.escamilla@outlook.fr>
2018-02-07 11:27:20 +0100

    Add getter and setter to user

--- API/src/main/java/org/api/entity/User.java
+++ API/src/main/java/org/api/entity/User.java
@@ -26,4 +26,46 @@ public class User implements Serializable {
     private String mail;
     
     private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
 }
