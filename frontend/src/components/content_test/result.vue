6eaa808351179ab75ca96ea67842365ce86b2a96
Bertrand <bertrandpenguilly@laposte.net>
2018-02-10 01:33:15 +0100

    fixed top score display

--- frontend/src/components/content_test/result.vue
+++ frontend/src/components/content_test/result.vue
@@ -9,8 +9,8 @@
           <li><div class="score">01 : <div class="left">{{item.nom}}</div> <div class="right">{{item.score}}</div></div></li>
 
         </div>-->
-      <ul   v-for="item in scores">
-        <li><div class="score"> <div class="left"><span class="strong">{{item.position}} :</span> {{item.nom}}</div> <div class="right">{{item.score}}</div></div></li>
+      <ul   v-for="(item, key) in scores">
+        <li><div class="score"> <div class="left"><span class="strong">{{key + 1}} :</span> {{item.nom}}</div> <div class="right">{{item.score}}</div></div></li>
       </ul>
       <button class="button is-primary" @click="home">Retour vers l'accueil</button>
     </div>
@@ -32,16 +32,10 @@ export default {
   },
 
   name: 'App',
-  data() {
-    return {
-    scores:[]
-
-  }
-},
   data: function () {
     return {
       finalScore :  "",
-
+      scores:[],
       topScore : json,
     };
 	},
@@ -49,8 +43,8 @@ export default {
     home(){
       this.$router.push({'name': 'home'})
     },
-    async getScores() {
-      await api.get('/score').then(response => {
+    getScores() {
+      api.get('/score').then(response => {
         // success callback
         this.scores = response.data
         console.log(this.scores)
